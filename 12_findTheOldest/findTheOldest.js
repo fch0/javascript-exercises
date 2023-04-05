3b1fa3f580065526e8b9709df0d25e58fba06d6f
fch0 <70779071+fch0@users.noreply.github.com>
2023-04-05 11:25:17 -0300

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+function findTheOldest(arr) {
+				if (arr.filter(people => !people.yearOfDeath).length == 0) return;
+				
+				const presentYear = new Date().getFullYear();
+				const peopleSortedByAge = arr.sort(function(a, b) {
+					
+					let aAge;
+					let bAge;
+					if (a.yearOfDeath === undefined) {
+						aAge = (presentYear - a.yearOfBirth);
+					} else {
+						aAge = (a.yearOfDeath - a.yearOfBirth);
+					}
 
-};
+					if (b.yearOfDeath === undefined) {
+						bAge = (presentYear - b.yearOfBirth);
+					} else {
+						bAge = (b.yearOfDeath - b.yearOfBirth);
+					}
+
+					if (aAge < bAge) {
+						return 1;
+					} else if (aAge > bAge) {
+						return -1;
+					} else if (aAge === bAge) {
+						return 0;
+					}
+				});
+				
+				if (!peopleSortedByAge[0].yearOfDeath) return peopleSortedByAge[0];
+				//return peopleSortedByAge;
+			}
 
 // Do not edit below this line
 module.exports = findTheOldest;
